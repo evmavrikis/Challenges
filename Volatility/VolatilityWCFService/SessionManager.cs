0b8bd999553a1306353d9ad5ba9cdc4dc5939998
Evangelos Mavrikis <evangelos.mavrikis@netcall.com>
2021-07-04 00:09:26 +0100

    Made notification broadcasting more targeted.

--- Volatility/VolatilityWCFService/SessionManager.cs
+++ Volatility/VolatilityWCFService/SessionManager.cs
@@ -11,8 +11,12 @@ using System.Configuration;
 
 namespace VolatilityWCFService
 {
+    
     class SessionManager
     {
+        // Concurrent dictionaries are not enitrely safe. They cannot protect us when we loop through a collection that is being modified.
+        private static readonly object _monitor = new object();
+
         private static ConcurrentDictionary<string, OperationContext> _sessionsById = new ConcurrentDictionary<string, OperationContext>();
         private static ConcurrentDictionary<int, CustomerDetails> _customerDetailsById = new ConcurrentDictionary<int, CustomerDetails>();
         static void Main(string[] args)
@@ -38,7 +42,75 @@ namespace VolatilityWCFService
             object[] ar = state as object[];
             var callback = (IVolatilityCallback)ar[0];
             var n = (Notification)ar[1];
-            callback.SendNotification(n);
+
+            // We broadcast to one or more clients depending on the norification type.
+            // If we detect that the session is lost we remove it from the dictionary.
+            // Must be careful with multithreading and when looping through the gloabal collection.
+            HashSet<string> sessionsToRemove = new HashSet<string>();
+            List<OperationContext> sessions = new List<OperationContext>();
+
+            // Lock to loop and createa local copy.
+            lock(_monitor)
+            {
+                sessions = _sessionsById.Values.ToList();
+            }
+
+            foreach (var ss in sessions)
+            {
+                var cb = ss.GetCallbackChannel<IVolatilityCallback>();
+                switch (n)
+                {
+                    case Notification.UnexpectedError:
+                        if (cb == callback)
+                        {
+                            // Notify only the caller for its errors.
+                            if (!TrySendNotification(cb,n))
+                            {
+                                sessionsToRemove.Add(ss.SessionId);
+                            }
+                        }
+                        break;
+                    default:
+                        if (cb != callback)
+                        {
+                            // Broadcast any data changes to the other clients.
+                            if (!TrySendNotification(cb, n))
+                            {
+                                sessionsToRemove.Add(ss.SessionId);
+                            }
+                        }
+
+                        break;
+                }
+            }
+            
+            // Lock to remove lost sessions.
+            if (sessionsToRemove.Count > 0)
+            {
+                lock(_monitor)
+                {
+                    foreach (var id in sessionsToRemove)
+                    {
+                        OperationContext o;
+                        _sessionsById.TryRemove(id, out o);
+                    }
+                }
+            }
+        }
+
+        // If there the session is lost then remove it from the sessions.
+        private static bool TrySendNotification(IVolatilityCallback callback,Notification n)
+        {
+            try
+            {
+                callback.SendNotification(n);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                // This is the exception we get when the session is lost.
+                return (ex.HResult == -2146233087);
+            }
         }
         internal static void LoadCustomerRecords()
         {
