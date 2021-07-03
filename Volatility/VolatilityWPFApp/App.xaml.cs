b4badd71cc718c4d40652266a28b917b5c55b8f7
Evangelos Mavrikis <evangelos.mavrikis@netcall.com>
2021-07-03 12:19:30 +0100

    Fix to always set the app arguments.

--- Volatility/VolatilityWPFApp/App.xaml.cs
+++ Volatility/VolatilityWPFApp/App.xaml.cs
@@ -17,10 +17,10 @@ namespace VolatilityWPFApp
         void app_Startup(object sender, StartupEventArgs e)
         {
             // If no command line arguments were provided, don't process them if (e.Args.Length == 0) return;  
-            if (e.Args.Length > 0)
-            {
+            //if (e.Args.Length > 0)
+            //{
                 Args = e.Args;
-            }
+            //}
         }
     }
 }
